c87e4c7aa8f74d548730f9df41ba0ca3a108bcf2
abenaessidedu <aessiedu98@gmail.com>
2026-08-01 23:58:43 -0400

    app

--- mvsba/src/App.jsx
+++ mvsba/src/App.jsx
@@ -1,9 +1,25 @@
 import React from "react";
-
+import { Browser as Router, Routes, Route } from "react-router-dom";
+import SearchBar from "./components/SearchBar";
+import MovieList from "./features/movieList";
+import FavoritesList from "./features/FavoritesList";
+import Navbar from "./components/NavBar";
 
 export default function App() {
   return (
-    <>
-    </>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={
+          <div>
+            <SearchBar />
+            <MovieList />
+          </div>
+        }
+
+        />
+        <Route path="/favorites" element={<FavoritesList />} />
+      </Routes>
+    </Router>
   )
 }
