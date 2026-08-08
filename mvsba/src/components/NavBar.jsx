5825efb416544f88d4fd2da54516e4b9456a13ed
abenaessidedu <aessiedu98@gmail.com>
2026-08-08 15:00:33 -0400

    gutted-fresh start

--- mvsba/src/components/NavBar.jsx
+++
@@ -1,8 +0,0 @@
-export default function Navbar() {
-    return (
-        <nav className="navbar"> 
-        <Link to="/"> HOME </Link>
-        <Link to="/favorites"> FAVORITES </Link>
-        </nav>
-    );
-};
