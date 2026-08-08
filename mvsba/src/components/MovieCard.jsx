e2ab625e6f899063882268d484b0415a0e1a5450
abenaessidedu <aessiedu98@gmail.com>
2026-08-08 18:07:55 -0400

    movie card

--- mvsba/src/components/MovieCard.jsx
+++ mvsba/src/components/MovieCard.jsx
@@ -3,8 +3,31 @@ import React from "react";
 
 export default function MovieCard() {
     return (
-       <a href="" className="movie_card">
-        <img src="https://www.washingtonpost.com/graphics/2019/entertainment/oscar-nominees-movie-poster-design/img/black-panther-web.jpg" alt="movie poster" className="movie_poster" /> 
-       </a>
-    )
-}
+        <a 
+        href="" 
+        className="movie_card">
+            <img 
+            src="https://www.washingtonpost.com/graphics/2019/entertainment/oscar-nominees-movie-poster-design/img/black-panther-web.jpg"
+                
+                alt="movie poster" 
+                className="movie_poster" />
+            <div className="movie_details">
+                <h3 className="movie_details_heading"> Movie Name </h3>
+                <div className="align_center movie_date_rate">
+                    {/* RELEASE DATE  */}
+
+                    <p> 02-16-2018 </p>
+                    {/* RATING  */}
+
+                    <p> 8.0 ⭐️ </p>
+                </div>
+                <p className="movie_description">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta nisi eveniet itaque non rem quo.
+                </p>
+            </div>
+
+
+
+        </a>
+    );
+}; 
--- mvsba/src/components/MovieList.jsx
+++ mvsba/src/components/MovieList.jsx
@@ -35,20 +35,7 @@ export default function MovieList() {
                 <MovieCard /> 
             </div>
 
-            <div className="movie_details">
-                <h3 className="movie_details_heading"> Movie Name </h3>
-            <div className="movie_date_rate">
-                           {/* RELEASE DATE  */}
-
-                <p> 02-16-2018 </p>
-                            {/* RATING  */}
-
-                <p> 8.0 ⭐️ </p>
-            </div>
-            <p className="movie_description"> 
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta nisi eveniet itaque non rem quo.
-            </p>
-            </div>
+        
 
         </section>
     )
