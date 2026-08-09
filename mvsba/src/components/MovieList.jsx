9c0073acc834c5f2b4e16f06c07801ce0c592487
abenaessidedu <aessiedu98@gmail.com>
2026-08-08 21:46:31 -0400

    styling

--- mvsba/src/components/MovieList.jsx
+++ mvsba/src/components/MovieList.jsx
@@ -1,13 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 import MovieCard from "./MovieCard";
 
 export default function MovieList() {
+
+useEffect(() => {
+    fetch("https://api.themoviedb.org/3/movie/popular?api_key=4f2a4b78035923567997eade8e69b198")
+}, []) 
+
+
     return (
     
         <section className="movie_list">
             {/* HEADER */}
             <header className="align_center  movie_list_header">
-                <h2 className="align_center movie_list_heading"> POP ULAR 🔥 </h2>
+                <h2 className="align_center movie_list_heading"> POPULAR 🔥 </h2>
                 
                 {/* FILTERING  */}
                 <div className="align_center movie_list_fs">
